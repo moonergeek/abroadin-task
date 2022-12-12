e8cec743ea8c87324eec5c273d420cb52ae0c741
mahan <mahanjafari14@gmail.com>
2022-12-13 00:11:22 +0330

    bug fixed

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./core/redux/store";
-import App from "./app/App";
+import App from "./App/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./main.css";
 
